00a85c1cd86cfa6d07ce1b26cd38964cd823754d
Alexey Lapin <a.lapin03@gmail.com>
2024-07-16 18:07:07 +0300

    llama.cpp support

--- fedot_util.py
+++ fedot_util.py
@@ -6,19 +6,21 @@ from fedot.core.data.data_split import train_test_data_setup
 from fedot.core.repository.tasks import TaskTypesEnum, Task
 from fedot.core.utils import fedot_project_root
 
+
 def run_example(train_df, test_df, dataset_metadata,
-                cv_folds = 10, metric = ['roc_auc', 'accuracy'], n_jobs = -1,
+                cv_folds=10, metric=['roc_auc', 'accuracy'], n_jobs=-1,
                 visualise: bool = False, with_tuning: bool = True,
                 timeout: float = 2., preset: str = 'auto'):
 
-    problem = dataset_metadata["task_type"].lower() 
+    problem = dataset_metadata["task_type"].lower()
 
     composer_params = {'history_dir': 'custom_history_dir', 'preset': preset}
-    auto_model = Fedot(problem=problem, seed=42, timeout=timeout, cv_folds = cv_folds,
-                       metric = metric, logging_level=logging.FATAL,
+    auto_model = Fedot(problem=problem, seed=42, timeout=timeout, cv_folds=cv_folds,
+                       metric=metric, logging_level=logging.FATAL,
                        with_tuning=with_tuning, **composer_params)
 
-    auto_model.fit(features=train_df, target=dataset_metadata['target_column'])
+    auto_model.fit(features=train_df,
+                   target=dataset_metadata['target_column'].capitalize())
     prediction = auto_model.predict(features=test_df)
 
     if visualise:
--- llm_util.py
+++ llm_util.py
@@ -1,24 +1,34 @@
 import os
 import json
+import requests
+
+MODEL_CHAT_ENDPOINT = os.getenv("MODEL_CHAT_ENDPOINT")
+if MODEL_CHAT_ENDPOINT is None:
+    raise ValueError("MODEL_CHAT_ENDPOINT environment variable is not set")
+
 
 def generate_dataset_split_description(dataset_description, dataset_columns):
     return f"The {dataset_description} split contains following columns: {dataset_columns}.\n"
 
+
 def generate_dataset_standard_description(dataset_metadata):
-    
+
     splits = dataset_metadata["splits"]
     train_split = splits[dataset_metadata["train_split_name"]]
     split_descriptions = dataset_metadata['split_descriptions']
-    
+
     text_columns = list(train_split.select_dtypes(include=['object']).columns)
-    numeric_columns = list(train_split.select_dtypes(include=['number']).columns)
-    
+    numeric_columns = list(
+        train_split.select_dtypes(include=['number']).columns)
+
     unique_count = train_split.apply(lambda col: col.nunique())
     unique_ratio = train_split.apply(lambda col: col.nunique() / len(col))
-    column_types = train_split.apply(lambda col: "string" if col.name in text_columns else "numeric")
-    
+    column_types = train_split.apply(
+        lambda col: "string" if col.name in text_columns else "numeric")
+
     column_descriptions = [f"{column_name}: {column_types[column_name]}"
-                           f"{100 * unique_ratio[column_name]:.2f}% unique values, examples: {list(train_split[column_name].head(10)) } "
+                           f"{100 * unique_ratio[column_name]:.2f}% unique values, examples: {
+                               list(train_split[column_name].head(10))} "
                            for column_name in train_split.columns]
 
     dataset_name = dataset_metadata['name']
@@ -26,10 +36,12 @@ def generate_dataset_standard_description(dataset_metadata):
     dataset_goal = dataset_metadata['goal']
     dataset_split_names = dataset_metadata['split_names']
 
-    split_description_lines = [generate_dataset_split_description(split_descriptions[split_name], list(splits[split_name].columns)) for split_name in dataset_split_names]
+    split_description_lines = [generate_dataset_split_description(split_descriptions[split_name], list(
+        splits[split_name].columns)) for split_name in dataset_split_names]
 
     introduction_lines = [
-        f"Assume we have a dataset called '{dataset_name}', which describes {dataset_description}, and the task is to {dataset_goal}.",
+        f"Assume we have a dataset called '{dataset_name}', which describes {
+            dataset_description}, and the task is to {dataset_goal}.",
         f""
     ] + split_description_lines + [
         f"Below is the type (numeric or string), unique value count and ratio for each column, and few examples of values:",
@@ -40,16 +52,17 @@ def generate_dataset_standard_description(dataset_metadata):
 
     return "\n".join(introduction_lines)
 
+
 def generate_categorical_definition_prompts(dataset_metadata):
 
     categorical_variable_info = [
-       f"A option feature column in a dataset:",
-       f"- Can have numeric or string values",
-       f"- Represents state or option, not highly varying quantitative or unique data",
-       f"- Has a low unique value ratio",
-       f"- Not necessaily ordered",
+        f"A option feature column in a dataset:",
+        f"- Can have numeric or string values",
+        f"- Represents state or option, not highly varying quantitative or unique data",
+        f"- Has a low unique value ratio",
+        f"- Not necessaily ordered",
     ]
-    
+
     task_lines = categorical_variable_info + [
         f"Your task is to return the list of all option feature columns.",
         f"Only answer with a column name on separate lines"
@@ -60,20 +73,27 @@ def generate_categorical_definition_prompts(dataset_metadata):
 
     return system_prompt, task_prompt
 
+
 def generate_target_definition_prompts(dataset_metadata):
-    
-    task_lines = [
-        f"Your task is to return the target column of the dataset",
-        f"Only answer with a column name"
-    ]
-    
-    system_prompt = generate_dataset_standard_description(dataset_metadata)
-    task_prompt = "\n".join(task_lines)
+
+    system_prompt = "\n".join(["Return the target column of the dataset. Output: only target column name.",
+                               "Q: Predict sales based on the money spent on different platforms for marketing.  colums: TV, Radio, Newspaper, Sales",
+                               "A: Sales",
+                               "Q: Predict the probability of various defects on steel plates. colums: X_Minimum, Y_Minimum, Pixels_Areas, Faults",
+                               "A: Faults"])
+
+    splits = dataset_metadata["splits"]
+    columns = ', '.join(
+        list(splits[dataset_metadata["train_split_name"]].columns))
+    task_prompt = '\n'.join([f"Q: {dataset_metadata['goal']}.",
+                            f"columns: {columns}",
+                            "A:"])
 
     return system_prompt, task_prompt
 
+
 def generate_task_definition_prompts(dataset_metadata):
-    
+
     task_lines = [
         f"Your task is to define whether the task is regression or classification",
         f"Only answer with a task type"
@@ -84,40 +104,38 @@ def generate_task_definition_prompts(dataset_metadata):
 
     return system_prompt, task_prompt
 
-def run_model_multicall(model, tokenizer, generation_config, dataset_metadata):
+
+def run_model_multicall(dataset_metadata):
     prompts = {
         "categorical_columns": generate_categorical_definition_prompts(dataset_metadata),
         "target_column": generate_target_definition_prompts(dataset_metadata),
         "task_type": generate_task_definition_prompts(dataset_metadata)
     }
-    
-    responses = {}
-    for task in prompts:
-        system_prompt, task_prompt = prompts[task]
-        messages = [
-            {"role": "system", "content": system_prompt},
-            {"role": "user", "content": task_prompt},
-        ]
-        
-        input_ids = tokenizer.apply_chat_template(
-            messages,
-            add_generation_prompt=True,
-            return_tensors="pt"
-        ).to(model.device)
-        
-        outputs = model.generate(
-            input_ids,
-            **generation_config
-        )
-        response = outputs[0][input_ids.shape[-1]:]
-        responses[task] = tokenizer.decode(response, skip_special_tokens=True)
 
+    return invoke_model(prompts)
+
+
+def invoke_model(prompts):
+    data_temp = {'model': "llama3"}
+    responses = {}
+    for key, value in prompts.items():
+        system_prompt, task_prompt = value
+        data = data_temp.copy()
+        data['messages'] = [
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": task_prompt}
+        ]
+        response = requests.post(MODEL_CHAT_ENDPOINT, json=data).json()
+        responses[key] = response['choices'][0]['message']['content']
     return responses
+
 
 def process_model_responses(responses):
-    responses["categorical_columns"] = responses["categorical_columns"].split("\n")
+    responses["categorical_columns"] = responses["categorical_columns"].split(
+        "\n")
     responses["task_type"] = responses["task_type"].lower()
     return responses
+
 
 def save_model_responses(responses, path):
     with open(os.sep.join([path, 'model_responses.json']), 'w') as json_file:
