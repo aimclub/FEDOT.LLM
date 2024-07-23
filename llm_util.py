6eacdf6f11051b7b237a5740916ed9f1992db654
Stanislav Chumakov <stas1f1@gmail.com>
2024-07-23 18:06:46 +0300

    Updated dataset init pipeline
    Added functionality to initialize the dataset from plain files and description, with pipeline filling all other fields iteratively

--- llm_util.py
+++ llm_util.py
@@ -1,64 +1,72 @@
 import os
 import json
-from tenacity import retry, stop_after_attempt, wait_random_exponential, retry_if_exception_type
-from requests.exceptions import RequestException
 
-_MAX_RETRIES = 6
-
-
-def run_model_multicall(model, tokenizer, generation_config, prompts):
-    """Run all prompts on local model
+def run_model_call(model, system, context, task):
+    """Run a prompt on model
     """
-    
+    model.set_sys_prompt(system)
+    model.add_context(context)
+    response = model(task, as_json=True)
+    return response
+
+def run_model_multicall(model, prompts):
+    """Run a list of prompts on web model
+    """
     responses = {}
     for task in prompts:
-        messages = [
-            {"role": "system", "content": prompts[task]["system"]},
-            {"role": "context", "content": prompts[task]["context"]},
-            {"role": "user", "content": prompts[task]["task"]},
-        ]
-        
-        input_ids = tokenizer.apply_chat_template(
-            messages,
-            add_generation_prompt=True,
-            return_tensors="pt"
-        ).to(model.device)
-        
-        outputs = model.generate(
-            input_ids,
-            **generation_config
+        response = run_model_call(
+            model = model,
+            system = prompts[task]["system"],
+            context = prompts[task]["context"],
+            task = prompts[task]["task"]
         )
-        response = outputs[0][input_ids.shape[-1]:]
-        responses[task] = tokenizer.decode(response, skip_special_tokens=True)
-
-    return responses
-
-@retry(
-    wait=wait_random_exponential(min=1, max=60),
-    stop=stop_after_attempt(_MAX_RETRIES),
-    retry=(retry_if_exception_type(RequestException) | retry_if_exception_type(RuntimeError)),
-    reraise=True
-)
-def run_web_model_multicall(model, prompts):
-    """Run all prompts on web model
-    """
-
-    responses = {}
-    for task in prompts:
-
-        model.set_sys_prompt(prompts[task]["system"])
-        model.add_context(prompts[task]["context"])
-        
-        response = model(prompts[task]["task"], as_json=True)
         responses[task] = response
-
     return responses
 
-def process_model_responses(responses):
-    responses["categorical_columns"] = responses["categorical_columns"].split("\n")
-    responses["task_type"] = responses["task_type"].lower()
+def process_model_responses(responses, operations):
+    for key in operations:
+        responses[key] = operations[key](responses[key])
+    return responses
+
+def process_model_responses_for_v1(responses):
+    operations = {
+        "categorical_columns": lambda x : x.split("\n"),
+        "task_type": lambda x : x.lower()
+    }
+    responses = process_model_responses(responses, operations)
     return responses
 
 def save_model_responses(responses, path):
     with open(os.sep.join([path, 'model_responses.json']), 'w') as json_file:
         json.dump(responses, json_file)
+
+
+
+# def run_model_multicall(model, tokenizer, generation_config, prompts):
+#     """Run all prompts on local model
+
+#     TODO: transform to an interaction with local model helper class
+#     """
+    
+#     responses = {}
+#     for task in prompts:
+#         messages = [
+#             {"role": "system", "content": prompts[task]["system"]},
+#             {"role": "context", "content": prompts[task]["context"]},
+#             {"role": "user", "content": prompts[task]["task"]},
+#         ]
+        
+#         input_ids = tokenizer.apply_chat_template(
+#             messages,
+#             add_generation_prompt=True,
+#             return_tensors="pt"
+#         ).to(model.device)
+        
+#         outputs = model.generate(
+#             input_ids,
+#             **generation_config
+#         )
+#         response = outputs[0][input_ids.shape[-1]:]
+#         responses[task] = tokenizer.decode(response, skip_special_tokens=True)
+
+#     return responses
--- prompts.py
+++ prompts.py
@@ -1,14 +1,29 @@
+dataset_name_prompt = '''Your task is to define the name of this dataset. Answer only with the name.'''
+
+dataset_description_prompt = '''Your task is to formulate a short description this dataset.
+It should be no longer than a paragraph'''
+
+train_split_definition_prompt = '''Your task is to define the train split of this dataset. Answer only with the name of the file with train split. Mind the register.'''
+
+target_split_definition_prompt = '''Your task is to define the target split of this dataset. Answer only with the name of the file with target split. Mind the register.'''
+
+dataset_goal_prompt = '''Your task is to formulate the task associated with this dataset. Answer only with the task description, mention the target column.
+It should be short, 1-2 sentences long.'''
+
+split_description_prompt = '''Your task is to describe the purpose of this dataset split.
+It should be short, 1-2 sentences long'''
+
 categorical_definition_prompt = '''Your task is to return the list of all option feature columns
                             Only answer with a column name on separate lines'''
 
-categorical_definition_context = ''' A option feature column in a dataset:
+categorical_definition_context = '''An option feature column in a dataset:
                             - Can have numeric or string values
                             - Represents state or option, not highly varying quantitative or unique data
                             - Has a low unique value ratio
                             - Not necessaily ordered'''
 
-target_definition_prompt = ''' Your task is to return the target column of the dataset
+target_definition_prompt = '''Your task is to return the target column of the dataset
                             Only answer with a column name'''
 
-task_definition_prompt = ''' Your task is to define whether the task is regression or classification
+task_definition_prompt = '''Your task is to define whether the task is regression or classification
                             Only answer with a task type'''
